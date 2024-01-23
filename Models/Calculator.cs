f4e491cc6b31541ee3fe3b9458a71d66faf88957
kaden-d <kaden-d@hotmail.com>
2024-01-23 10:46:55 -0700

    removal of form in TipCalculatorView() and adding onclick to button in same view

--- Models/Calculator.cs
+++ Models/Calculator.cs
@@ -3,7 +3,7 @@
     public class Calculator
     {
         public double Add(double a, double b) => a + b;
-        public double Subtract(double a, double b) =>a - b;
+        public double Subtract(double a, double b) => a - b;
         public double Multiply(double a, double b) => a * b;
         public double Divide(double a, double b)
         {
