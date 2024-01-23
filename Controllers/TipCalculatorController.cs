66734d7c5ffbb8b62779711b47a66622122e93ce
kaden-d <kaden-d@hotmail.com>
2024-01-23 11:24:23 -0700

    attemting to obtain values from TipCalculatorView into TipCalculatorController and return outputs back to View

--- Controllers/TipCalculatorController.cs
+++ Controllers/TipCalculatorController.cs
@@ -9,5 +9,24 @@ namespace CPRO2211CalculatorProject.Controllers
         public IActionResult TipCalculatorView()
         {
             return View();
-        }    }
+        }
+        [HttpPost]
+        public IActionResult TipCalculatorView(TipCalculator tc)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Tip15 = tc.Tip();
+                ViewBag.Tip20 = tc.Tip();
+                ViewBag.Tip25 = tc.Tip();
+            }
+            else
+            {
+                ViewBag.Tip15 = 0;
+                ViewBag.Tip20 = 0;
+                ViewBag.Tip25 = 0;
+                ViewBag.Total = 0;
+            }
+            return View(tc);
+        }
+    }
 }
