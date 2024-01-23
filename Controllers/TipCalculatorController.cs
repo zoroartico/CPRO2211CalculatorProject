4b0c94e5d37b5a32a7790668e9ffa5d597e2d82b
kaden-d <kaden-d@hotmail.com>
2024-01-23 10:28:58 -0700

    various changes

--- Controllers/TipCalculatorController.cs
+++ Controllers/TipCalculatorController.cs
@@ -1,7 +1,6 @@
 ﻿using CPRO2211CalculatorProject.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-
 namespace CPRO2211CalculatorProject.Controllers
 {
     public class TipCalculatorController : Controller
