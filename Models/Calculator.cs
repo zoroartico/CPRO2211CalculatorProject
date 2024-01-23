3bdffc71d344f9ed8b1fb43d10eeba5257c25b41
Syyren <129496054+Syyren@users.noreply.github.com>
2024-01-23 10:56:30 -0700

    updated Calculator to allow indefinite params.

--- Models/Calculator.cs
+++ Models/Calculator.cs
@@ -1,16 +1,51 @@
-﻿namespace CPRO2211CalculatorProject.Models
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace CPRO2211CalculatorProject.Models
 {
     public class Calculator
     {
-        //creating a variety of methods that return the result of two numbers after performing mathmatical operations on them.
-        public double Add(double a, double b) => a + b;
-        public double Subtract(double a, double b) => a - b;
-        public double Multiply(double a, double b) => a * b;
-        public double Divide(double a, double b)
+        //creating a variety of methods that return the result of a varying qty of numbers after
+        //performing mathmatical operations on them.
+        public double Add(params double[] numbers)
         {
-            if (b == 0)
-                throw new ArgumentException("Divide by 0 error.");
-            return a / b;
+            double result = 0;
+            foreach (double number in numbers)
+            {
+                result += number;
+            }
+            return result;
+        }
+        public double Subtract(params double[] numbers)
+        {
+            double result = 0;
+            foreach (double number in numbers)
+            {
+                result -= number;
+            }
+            return result;
+        }
+        public double Multiply(params double[] numbers)
+        {
+            double result = 1;
+            foreach (double number in numbers)
+            {
+                result *= number;
+            }
+            return result;
+        }
+        public double Divide(params double[] numbers)
+        {
+            double result = 0;
+            foreach (double number in numbers)
+            {
+                if (number == 0)
+                    throw new ArgumentException("Divide by 0 error.");
+                else if (result == 0)
+                    result = number;
+                else
+                    result /= number;
+            }        
+            return result;
         }
     }
 }
