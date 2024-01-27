126d3257082675f7f7999b23acfec2365b52bdf1
Syyren <129496054+Syyren@users.noreply.github.com>
2024-01-27 10:59:02 -0700

    updated error messages.

--- ErrorMsg.cs
+++ ErrorMsg.cs
@@ -3,7 +3,9 @@
     public class ErrorMsg
     {
         //this class contains error messages to raise in the application.
-        public const string EMPTY = "Cannot process less than one number. Please provide at least one number.";
+        public const string EMPTY = "Cannot process an empty input. Please enter something.";
         public const string DIV_0 = "Cannot divide a number by zero. Please ensure parameters do not equal zero.";
+        public const string LESS_0 = "Entered number cannot be less than zero.";
+        public const string RANGE_0_100 = "Entered number must be between 0 and 100";
     }
 }
--- Models/PriceQuotationModel.cs
+++ Models/PriceQuotationModel.cs
@@ -1,9 +1,17 @@
-﻿namespace CPRO2211CalculatorProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPRO2211CalculatorProject.Models
 {
     public class PriceQuotationModel : CalculatorModel
     {
         //setting attributes of the model
+        [Required(ErrorMessage = ErrorMsg.EMPTY)]
+        [Range(0, 999999999999, ErrorMessage = ErrorMsg.LESS_0)]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = ErrorMsg.EMPTY)]
+        [Range(0, 100, ErrorMessage = ErrorMsg.RANGE_0_100)]
+
         public decimal DiscountPercent { get; set; }
         //method to get the total price after discount
         public decimal Total() => Multiply(Price, DiscountPercent);
