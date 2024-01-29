d8770fd8d924e3f9d227ff0daffcdd9d908a38d8
kaden-d <kaden-d@hotmail.com>
2024-01-29 14:11:44 -0700

    Removed new instance on HTTPGET for TipcalcualtorView, modified tipcalculatorview to be more consistant with PriceQuotationView

--- Controllers/TipCalculatorController.cs
+++ Controllers/TipCalculatorController.cs
@@ -5,18 +5,21 @@ namespace CPRO2211CalculatorProject.Controllers
 {
     public class TipCalculatorController : Controller
     {
+        //Declaring default route as const
         private const string DEF = "tip";
-
+        //routing the pathway to /tip
         [Route(DEF)]
+        //setting the get request for the web server
         [HttpGet]
         public IActionResult TipCalculatorView()
         {
             ViewBag.Tip15 = 0;
             ViewBag.Tip20 = 0;
             ViewBag.Tip25 = 0;
-            return View( new TipCalculatorModel() );
+            return View();
         }
-
+        //setting the post request for the web server. This will activate on button click in the web form
+        //if the request is valid, then it will update the tip fields
         [Route(DEF)]
         [HttpPost]
         public IActionResult TipCalculatorView(TipCalculatorModel tc)
@@ -26,6 +29,12 @@ namespace CPRO2211CalculatorProject.Controllers
                 ViewBag.Tip15 = tc.Tip(15m);
                 ViewBag.Tip20 = tc.Tip(20m);
                 ViewBag.Tip25 = tc.Tip(25m);
+            } 
+            else
+            { 
+                ViewBag.Tip15 = 0; 
+                ViewBag.Tip20 = 0; 
+                ViewBag.Tip25 = 0; 
             }
             return View(tc);
         }
